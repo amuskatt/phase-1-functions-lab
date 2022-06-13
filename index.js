a9560482b473d812b57d97b72defa8c4b4902984
amuskatt <102235034+amuskatt@users.noreply.github.com>
2022-06-12 21:03:26 -0400

    commit tree 129f1141be28370ff599f58dbe9a0c1d642901fc

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const headQuarters = 42
+   const block = 264
+function distanceFromHqInBlocks(street) {
+    if (headQuarters < street){
+    return (street) - (headQuarters);
+    }
+    else {
+        return (headQuarters) - (street);
+    }
+  }
+  
+  function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * block;
+  }
+
+  function distanceTravelledInFeet(a, b) {
+      if (a > b){
+      return (a - b) * block;
+      }
+      else {
+          return (b - a) * block;
+      }
+ }
+
+  function calculatesFarePrice(start, destination){
+      let journey = distanceTravelledInFeet(start, destination)
+      if (journey <= 400 ){
+          return 0
+      }
+      else if (journey > 400 && journey <= 2000){
+          return (journey - 400) * .02
+      }
+      else if (journey >= 2000 && journey <= 2500){
+          return 25
+      }
+      else if (journey > 2500){
+          return 'cannot travel that far'
+      }
+    }
